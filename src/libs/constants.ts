8ba3d31bd8271b9746dbafe3089808a2396878e6
SahilMahadwar <accounts@mahadwar.com>
2024-09-21 10:13:03 +0530

    disable devetools in production

--- src/libs/constants.ts
+++ src/libs/constants.ts
@@ -1,3 +1,5 @@
+export const envMode = import.meta.env.MODE;
+
 export const baseApiUrl = import.meta.env.VITE_API_URL;
 
 export const queryKeys = {
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,24 @@
 import { QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { App as AntApp, ConfigProvider } from "antd";
+import React, { Suspense } from "react";
 import { antTheme } from "../theme/ant-theme";
 
+import { envMode } from "../libs/constants";
 import "../theme/globals.scss";
+
+const TanStackRouterDevtools =
+  envMode === "production"
+    ? () => null // Render nothing in production
+    : React.lazy(() =>
+        // Lazy load in development
+        import("@tanstack/router-devtools").then((res) => ({
+          default: res.TanStackRouterDevtools,
+          // For Embedded Mode
+          // default: res.TanStackRouterDevtoolsPanel
+        }))
+      );
 
 interface RouterContext {
   queryClient: QueryClient;
@@ -22,7 +35,9 @@ export const Route = createRootRouteWithContext<RouterContext>()({
 
       <ReactQueryDevtools />
 
-      <TanStackRouterDevtools />
+      <Suspense>
+        <TanStackRouterDevtools />
+      </Suspense>
     </>
   ),
 });
